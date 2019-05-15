55e21146002116eb19c4b2012c07bd044b3593b2
Noelle Leigh <noah.d.leigh@gmail.com>
2019-05-15 13:09:55 -0400

    Removed dependency from New-Plugin-Bundle.ps1

--- New-Plugin-Bundle.ps1
+++ New-Plugin-Bundle.ps1
@@ -3,4 +3,4 @@ if (Test-Path .\woocommerce-cart-rules.zip) {
     Remove-Item .\woocommerce-cart-rules.zip;
 }
 # Make a new one
-7z a woocommerce-cart-rules.zip .\woocommerce-cart-rules.php .\readme.txt;
+Compress-Archive -Path .\woocommerce-cart-rules.php, .\readme.txt, .\LICENSE -DestinationPath .\woocommerce-cart-rules.zip;
